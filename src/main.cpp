4e9f4d4fefaf32bc9478e8c74b27eb105fb7d28c
sfazailh <105238625+sfazailh@users.noreply.github.com>
2022-05-21 02:38:39 +0500

    Handling timer auxiliary variables
    as well as correcting comments for pin number for sensors and leds

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,15 @@
 #define blinkTime 3
 
 const int enterGreenLED = 26; //setting output signal to Green LED for entry at pin 26
-const int leaveRedLED = 33; //setting output signal to Green LED for entry at pin 33
+const int leaveRedLED = 33; //setting output signal to Red LED for leave at pin 33
 const int entrySensor = 27; //setting input signal from entry PIR Motion Sensor at pin 27
-const int ledPin = 25; //setting input signal from entry PIR Motion Sensorat pin 25
+const int leaveSensor = 25; //setting input signal from leave PIR Motion Sensorat pin 25
 
-
+//auxiliary variables for timer to be used for interrupt handling
+unsigned long currentTime = millis(); // millis() returns the number of milliseconds that have passed since the program upload to ESP-32 was completed
+unsigned long lastEnterTriggerTime = 0; //to record the time at which the last Entry PIR Motion sensor trigger occurred
+unsigned long lastLeaveTriggerTime = 0; //to record the time at which the last Leave PIR Motion sensor trigger occurred
+boolean startTimer = false;
 
 void setup() {
   // setup pin 5 as a digital output pin
