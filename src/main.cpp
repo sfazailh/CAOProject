5b7e5ac6809315768dfb893676ae200f7c4cd3cb
sfazailh <105238625+sfazailh@users.noreply.github.com>
2022-05-21 02:30:37 +0500

    Defining time for led blinks and adding comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 
 #define blinkTime 3
 
-const int enterGreenLED = 26;
-const int leaveRedLED = 33;
-const int entrySensor = 27;
-const int ledPin = 25;
+const int enterGreenLED = 26; //setting output signal to Green LED for entry at pin 26
+const int leaveRedLED = 33; //setting output signal to Green LED for entry at pin 33
+const int entrySensor = 27; //setting input signal from entry PIR Motion Sensor at pin 27
+const int ledPin = 25; //setting input signal from entry PIR Motion Sensorat pin 25
+
+
 
 void setup() {
   // setup pin 5 as a digital output pin
